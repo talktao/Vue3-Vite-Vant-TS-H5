321109bbcff6e968bff852b715680e0fa8693b74
talktao <t1416249906@163.com>
2022-03-24 22:14:19 +0800

    fix：增加自动化路由注释及清理console.log

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
+// 通过Vite的import.meta.glob()方法实现自动化导入路由
 const mainRouterModules = import.meta.glob('../layout/*.vue')
-console.log(mainRouterModules,'mainRouterModules');
-
 const viewRouterModules = import.meta.glob('../views/**/*.vue')
-console.log(viewRouterModules,'viewRouterModules');
-
 
 // 子路由
-const childRoutes = Object.keys(viewRouterModules).map((path)=>{
-  const childName = path.match(/\.\.\/views\/(.*)\.vue$/)[1].split('/')[1];
-  return {
-      path: `/${childName.toLowerCase()}`,
-      name: childName,
-      component: viewRouterModules[path]
-  } 
+const childRoutes = Object.keys(viewRouterModules).map((path)=>{	
+	const childName = path.match(/\.\.\/views\/(.*)\.vue$/)[1].split('/')[1];
+	return {
+		path: `/${childName.toLowerCase()}`,
+		name: childName,
+		component: viewRouterModules[path]
+	} 
 })
 
 console.log(childRoutes,'childRouter');
@@ -22,76 +19,20 @@ console.log(childRoutes,'childRouter');
 // 根路由
 const rootRoutes = Object.keys(mainRouterModules).map((path) => {
     const name = path.match(/\.\.\/layout\/(.*)\.vue$/)[1].toLowerCase();
-    console.log(name,'name');
     const routePath = `/${name}`;
-    console.log(routePath,'routePath');
     if (routePath === '/index') {
-      return {
-        path: '/',
-        name,
-        redirect: '/home',
-        component: mainRouterModules[path],
-        children: childRoutes
-      };
+		return {
+			path: '/',
+			name,
+			redirect: '/home',
+			component: mainRouterModules[path],
+			children: childRoutes
+		};
     }
 })
 
-
-
 const routes: Array<RouteRecordRaw> = rootRoutes
 
-
-// const routes: Array<RouteRecordRaw> = [
-//    {
-//         path: '/',
-//         name: 'Index',
-//         component: () => import ('@/layout/index.vue'),
-//         redirect: '/home',
-//         meta: {
-//             title: '首页',
-//             keepAlive:false
-//         },
-//         children: [
-//             {
-//                 path: '/home',
-//                 name: 'Home',
-//                 component: () => import('@/views/home/Home.vue')
-//             },
-//             {
-//                 path: '/category',
-//                 name: 'Category',
-//                 component: () => import('@/views/category/Category.vue')
-//             },
-//             {
-//                 path: '/welfare',
-//                 name: 'Welfare',
-//                 component: () => import('@/views/welfare/Welfare.vue')
-//             },
-//             {
-//                 path: '/orders',
-//                 name: 'Orders',
-//                 component: () => import('@/views/orders/Orders.vue')
-//             },
-//             {
-//                 path: '/about',
-//                 name: 'About',
-//                 component: () => import('@/views/about/About.vue')
-//             },
-//             {
-//                 path: '/orders',
-//                 name: 'Orders',
-//                 component: () => import('@/views/order/Orders.vue')
-//             },
-//             {
-//                 path: '/detail/:id',
-//                 name: 'Detail',
-//                 component: () => import('@/views/home/Detail.vue')
-//             }
-//         ]
-//     },    
-// ]
-
-console.log(routes,'routes');
 
 
 const router = createRouter({
@@ -100,3 +41,10 @@ const router = createRouter({
 });
 
 export default router
+
+
+
+
+
+
+
