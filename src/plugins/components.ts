b50125d6f308623282e91680e00fafb583b034c8
tantao <1416249906@qq.com>
2022-08-25 14:43:02 +0800

    fix：匹配conponents路径下的子文件夹里的.vue文件

--- src/plugins/components.ts
+++ src/plugins/components.ts
@@ -1,16 +1,16 @@
-const modules = import.meta.globEager('../components/*.vue')
+const modules = import.meta.globEager('../components/*/*.vue');
 
 export default {
     install(app) {
         Object.keys(modules).forEach(componentPath => {
 
             // 获取遍历的当前组件实例对象
-            let curComponent = modules[componentPath]?.default 
+            let curComponent = modules[componentPath]?.default;
 
             app.component(curComponent.name, curComponent);
-        })
+        });
     }
-   
+
 }
 
 
