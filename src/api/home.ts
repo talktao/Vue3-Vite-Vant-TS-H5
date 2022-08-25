25dd21101c43a8981922113d470cd18334134474
talktao <t1416249906@163.com>
2022-08-25 21:09:17 +0800

    remove

--- src/api/home.ts
+++
@@ -1,5 +0,0 @@
-import axiosInstance, { AxiosResponseProps } from '@/uitls/request';
-
-export const getList = (params?: any) => {
-	return axiosInstance.get("https://www.fastmock.site/mock/826b7b3ffc420fe885bdce88c8e6cdb7/test/api/list", { params: params || {} });
-};
--- src/api/orders.ts
+++
@@ -1,5 +0,0 @@
-import axiosInstance, { AxiosResponseProps } from '@/uitls/request'
-
-export const getList = (params: any) => {
-	return axiosInstance.get("/common/code/logisticsInfo/getLogisticsByOrderId", { params: params || {} });
-}
