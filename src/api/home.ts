85b425f3b38cabe4c11095ddc3a0d0bbff09fbdf
talktao <t1416249906@163.com>
2022-08-25 21:04:57 +0800

    fix: ci

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,5 +1,5 @@
 import axiosInstance, { AxiosResponseProps } from '@/uitls/request';
 
 export const getList = (params?: any) => {
-	return axiosInstance.get("http://yapi.smart-xwork.cn/mock/60279/bear/tabs", { params: params || {} });
+	return axiosInstance.get("https://www.fastmock.site/mock/826b7b3ffc420fe885bdce88c8e6cdb7/test/api/list", { params: params || {} });
 };
