2ef5ad0f03e749b017b438f6ff98be32df2cc79b
tantao <1416249906@qq.com>
2022-08-25 14:41:15 +0800

    feat：配置host

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,44 @@
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path"
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from "path";
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode})=>{
+export default defineConfig(({ command, mode }) => {
 	// 检查process.cwd()路径下.env.develeport.local、.env.development、.env.local、.env这四个环境文件
-	loadEnv(mode, process.cwd()) 
+	loadEnv(mode, process.cwd());
 	return {
-		
+
 		// 静态资源基础路径 base: './' || '',
-		base:'./',
-		
+		base: './',
+
 		resolve: {
-			alias:{
+			alias: {
 				// 配置src目录
-				"@": path.resolve(__dirname,"src"),
+				"@": path.resolve(__dirname, "src"),
 				// 导入其他目录
 				"components": path.resolve(__dirname, "components")
 			}
 		},
 		plugins: [
-				vue(),
-				// 配置后，Vant各组件才生效
-				styleImport({
-					resolves: [VantResolve()],
-				}),
+			vue(),
+			// 配置后，Vant各组件才生效
+			styleImport({
+				resolves: [VantResolve()],
+			}),
 		],
 
-		
-	
+
+
 		// 跨域代理
-		server:{
-			proxy:{
+		server: {
+			host: '0.0.0.0',
+			proxy: {
 				//这里是通过请求/api 来转发到 https://api.pingping6.com/
 				//假如你要请求https://api.*.com/a/a
 				//那么axios的url，可以配置为 /api/a/a
 				'/api': ''
-			}   
+			}
 		}
-	}	
-})
+	};
+});
